68b9f53f80b70d33f60d0077e048d240ccd823d7
Mahyar <mahyar.eb@gmail.com>
2023-05-07 14:41:56 -0700

    login page and create user page

--- src/Controller/controller.java
+++ src/Controller/controller.java
@@ -2,16 +2,26 @@ package Controller;
 import java.awt.CardLayout;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import contentPanels.homePanel;
 import contentPanels.roiPanel;
 import contentPanels.accountPanel;
 import contentPanels.uploadPanel;
+import contentPanels.loginPanel;
+import contentPanels.createUserPanel;
+
+import Objects.loginObject;
+
 
 public class controller extends JPanel{
 
     private static controller instance;
     private static JFrame frame;
+    private String username;
+    private Map<String, String> credentials;
 
     JPanel cards;//create panel of cards
 
@@ -20,18 +30,25 @@ public class controller extends JPanel{
     uploadPanel photoPanel;
     roiPanel roiPanel;
     accountPanel accountPanel;
+    loginPanel loginPanel;
+    createUserPanel createUserPanel;
 
     public controller(){
+        credentials = new HashMap<>();
         setLayout(new BorderLayout());
-        setSize(500, 500);
+        setSize(300, 300);
         cards = new JPanel(new CardLayout());
 
+        loginPanel = new loginPanel();
+        createUserPanel = new createUserPanel();
         homePanel = new homePanel();
         homePanel.startSlideshow();//statting home slide show when program is opened 
         photoPanel = new uploadPanel();
         roiPanel = new roiPanel();
         accountPanel = new accountPanel();
         
+        cards.add(loginPanel, "Login");
+        cards.add(createUserPanel, "Create User");
         cards.add(homePanel, "Homescreen");
         cards.add(photoPanel, "Upload Photos");
         cards.add(roiPanel, "ROI Table");
@@ -50,6 +67,13 @@ public class controller extends JPanel{
         instance = new controller();
 
         frame.getContentPane().add(instance);
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void loginToHome() {
+        controller.getInstance().changeCard("Homescreen");
         frame.setSize(1200, 1000);
         frame.setVisible(true);
     }
@@ -58,10 +82,51 @@ public class controller extends JPanel{
         createAndDisplay();
     }
 
-    //to move between panels 
+    // To move between panels 
     public void changeCard(String card){
         CardLayout c1 = (CardLayout) (cards.getLayout());
         c1.show(cards, card);
+
+    }
+
+    // Login the user
+    public loginObject loginUser(String username, String password){
+        String encryptedPassword = Base64.getEncoder().encodeToString(password.getBytes());
+        // check user
+        if(!credentials.containsKey(username)){
+            return new loginObject(false, "User does not exist");
+        }
+        // check password
+        if(!encryptedPassword.equals(credentials.get(username))){
+            return new loginObject(false, "Incorrect Password");
+        }
+        this.username = username;
+        homePanel.setNewUser(username);
+        return new loginObject(true);
+    }
+
+    public void logOut(){
+        controller.getInstance().changeCard("Login");
+        frame.setSize(300, 300);
+        this.username = "";
+    }
+
+    // Create a user and encrypt the credentials
+    public loginObject createUser(String username, String password){
+        if(credentials.containsKey(username)){
+            // username is taken
+            return new loginObject(false,"Username is taken");
+        }
+        if(password.length() < 6){
+            return new loginObject(false,"Password is not long enough. Please add at least 6 characters");
+        }
+        String encryptedPassword = Base64.getEncoder().encodeToString(password.getBytes());
+        credentials.put(username, encryptedPassword);
+        return new loginObject(true);
+    }
+
+    public String getUser(){
+        return username;
     }
 
     public static controller getInstance(){
---
+++ src/contentPanels/loginPanel.java
@@ -0,0 +1,123 @@
+package contentPanels;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import UI_Formatter.fileUIController;
+import UI_Formatter.colorPalette;
+import Controller.controller;
+
+import Objects.loginObject;
+
+public class loginPanel extends JPanel{
+
+    private static fileUIController fileUIController = new fileUIController();
+    JTextField usernameField;
+    JPasswordField passwordField;
+    JLabel wrongPasswordLabel;
+    
+
+    public loginPanel(){
+
+
+        //middle panel containing most of the content 
+        JPanel middlePanel = new JPanel();
+        middlePanel.setPreferredSize(new Dimension(850, 1000));
+        middlePanel.setBackground(colorPalette.background);
+        bottomMiddle(middlePanel);
+
+        //setting the panels layout to border layout
+        setLayout(new BorderLayout());
+
+        //creatting a wrapper to layer the middle panels 
+        JPanel wrapperPanel = new JPanel();
+        wrapperPanel.setLayout(new BoxLayout(wrapperPanel, BoxLayout.Y_AXIS));
+
+        wrapperPanel.add(middlePanel);
+
+        //adding panels to format the main panel
+        add(wrapperPanel, BorderLayout.CENTER);
+    }
+
+    
+    /**
+     * Designing the main content of the panel
+     * @param middlePanel where content should be added
+     */
+    private void bottomMiddle(JPanel middlePanel){
+
+        middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.Y_AXIS));
+
+        // Create the username label and input field
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameField = new JTextField();
+        usernameField.setMaximumSize(new Dimension(300, 30)); // Set the preferred size to be 300x30 pixels
+        middlePanel.add(usernameLabel);
+        middlePanel.add(usernameField);
+
+        // Create the password label and input field
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordField = new JPasswordField();
+        passwordField.setMaximumSize(new Dimension(300, 30)); // Set the preferred size to be 300x30 pixels
+        middlePanel.add(passwordLabel);
+        middlePanel.add(passwordField);
+
+        // Label to show when password is wrong
+        wrongPasswordLabel = new JLabel("");
+        wrongPasswordLabel.setVisible(false);
+        wrongPasswordLabel.setForeground(new Color(255, 0, 0));
+        middlePanel.add(wrongPasswordLabel);
+
+        // Create the submit button and attach an ActionListener to call the checkPassword function
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                checkPassword();
+            }
+        });
+        middlePanel.add(submitButton);
+
+        // Create a new User
+        JLabel signUpLabel = new JLabel();
+        signUpLabel.setText("<html><u><b><font color='blue'><br>New User? Sign Up!</font></b></u></html>");
+        signUpLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                resetLoginUI();
+                controller.getInstance().changeCard("Create User");
+            }
+        });
+        middlePanel.add(signUpLabel);
+    }
+
+    private void resetLoginUI(){
+        wrongPasswordLabel.setVisible(false);
+        wrongPasswordLabel.setText("");
+        usernameField.setText("");
+        passwordField.setText("");
+    }
+
+    /**
+     * Calling instance of fileUIController to change the card
+     */
+    public static loginObject login(String username, String password){
+        return controller.getInstance().loginUser(username, password);
+    }
+
+    private void checkPassword() {
+        String username = usernameField.getText();
+        String password = new String(passwordField.getPassword());
+        loginObject login = login(username, password);
+        if(login.isSuccess()){
+            resetLoginUI();
+            fileUIController.changeCard("No Files");
+            controller.loginToHome();
+        } else {
+            // show fail message
+            wrongPasswordLabel.setVisible(true);
+            wrongPasswordLabel.setText(login.getMessage());
+        }
+    }
+
+}
